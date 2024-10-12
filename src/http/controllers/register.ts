5d2feac8c01b7a75006610f2338235f17ed163cb
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-10-12 14:18:09 -0300

    feat: Implement user registration use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
-import { hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import z from 'zod'
+import { z } from 'zod'
 
-import { prisma } from '@/lib/prisma'
+import { registerUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,25 +12,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const passwordHash = await hash(password, 6)
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
+    if (error instanceof Error) {
+      return reply.status(409).send({ message: error.message })
+    }
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send()
+    throw error
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: passwordHash,
-    },
-  })
 
   return reply.status(201).send()
 }
---
+++ src/use-cases/register.ts
@@ -0,0 +1,37 @@
+import { hash } from 'bcryptjs'
+
+import { prisma } from '@/lib/prisma'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const passwordHash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('User with same email already exists.')
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: passwordHash,
+    },
+  })
+
+  return user
+}
