72f52837e6c54b8451e9aa7d4b0aa48172a3046e
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-22 19:12:40 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,18 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfileUseCase = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+  const { user } = await getUserProfileUseCase.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined, // Remove password_hash from response
+    },
+  })
 }
