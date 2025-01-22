96e130ba0fa733ca4514da0463687e9d2cdaedba
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-22 19:02:58 -0300

    Controllers & Testes E2E - Implementando JWT no Fastify

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,10 +3,10 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(['development', 'production', 'test'])
-    .default('development'),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
