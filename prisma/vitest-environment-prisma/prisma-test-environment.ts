399f08ebcf8aedd0cfb08590d64feb0a3359f341
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-23 11:05:48 -0300

    Controllers & Testes E2E - Teste E2E do registro

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,4 +1,26 @@
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+import { PrismaClient } from '@prisma/client'
 import { ContextTestEnvironment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error(
+      'DATABASE_URL is not set. Please set the DATABASE_URL environment variable.',
+    )
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <ContextTestEnvironment>{
   name: 'prisma',
@@ -6,11 +28,21 @@ export default <ContextTestEnvironment>{
   options: {},
 
   async setup() {
-    console.log('Executing setup...')
+    const schema = randomUUID()
+
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('Executing teardown...')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
