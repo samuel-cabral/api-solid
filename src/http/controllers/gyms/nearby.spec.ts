a41a6ea0aae66cd10b2efd4cc300d6d6697433b2
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-24 11:23:22 -0300

    Controllers & Testes E2E - Testes E2E de rotas de academias

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,59 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near Gym',
+        description: null,
+        phone: null,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far Gym',
+        description: null,
+        phone: null,
+        latitude: -27.0610928,
+        longitude: -49.5229501,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    console.log('🚀 ~ it ~ response:', response.body)
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Near Gym',
+      }),
+    ])
+  })
+})
