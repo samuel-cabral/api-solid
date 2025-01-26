f24099025ea5a3476bfa82ec85ef573e6cd4b686
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-26 18:13:58 -0300

    Refresh Token & RBAC - Autorização por cargos (RBAC)

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 import { create } from './create'
 import { history } from './history'
@@ -11,7 +12,11 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
--- src/http/controllers/gyms/create.ts
+++ src/http/controllers/gyms/create.ts
@@ -21,7 +21,7 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
 
   const createGymUseCase = makeCreateGymUseCase()
 
-  await createGymUseCase.execute({
+  const { gym } = await createGymUseCase.execute({
     title,
     description,
     phone,
@@ -29,5 +29,5 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     longitude,
   })
 
-  return reply.status(201).send()
+  return reply.status(201).send({ gym })
 }
