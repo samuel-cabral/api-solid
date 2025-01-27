2af7cdd3b9acc9a4184f24a0695350c488448830
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-27 10:34:51 -0300

    Refresh Token & RBAC - Testes de RBAC

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -24,7 +24,7 @@ describe('Profile (e2e)', () => {
     expect(profileResponse.statusCode).toEqual(200)
     expect(profileResponse.body.user).toEqual(
       expect.objectContaining({
-        email: 'john.doe@example.com',
+        email: 'johndoe@example.com',
       }),
     )
   })
--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,19 +1,30 @@
-import type { FastifyInstance } from 'fastify'
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    password: '123456',
+import { prisma } from '@/lib/prisma'
+
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
-    email: 'john.doe@example.com',
+    email: 'johndoe@example.com',
     password: '123456',
   })
 
   const { token } = authResponse.body
 
-  return { token }
+  return {
+    token,
+  }
 }
